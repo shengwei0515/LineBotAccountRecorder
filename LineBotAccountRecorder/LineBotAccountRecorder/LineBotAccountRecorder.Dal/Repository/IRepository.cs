8d24656fb87fb20bb0c42b28acd8c4fe803da71b
shengwei0515 <small_seal@ShaneSyu.local>
2021-07-06 00:54:42 +0800

    feat: add dal layer

--- LineBotAccountRecorder/LineBotAccountRecorder/LineBotAccountRecorder.Dal/Repository/IRepository.cs
+++ LineBotAccountRecorder/LineBotAccountRecorder/LineBotAccountRecorder.Dal/Repository/IRepository.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+//using LineBotAccountRecorder.Core.Utils.Specification;
 namespace LineBotAccountRecorder.Dal.Repository
 {
-    public interface IRepository
+    public interface IRepository<TEntity>
     {
+        void Create(TEntity entity);
+
+        //TEntity FindOne(ISpecification<TEntity> spec);
+
+
     }
 }
