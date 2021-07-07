caee6dc08d3d40a45b7befc433ca6731a59e3c0e
shengwei0515 <small_seal@ShaneSyu.local>
2021-07-08 01:27:00 +0800

    feat: add basic structure: ExceptionHandler, Dbcontext, Swagger

---
+++ LineBotAccountRecorder/LineBotAccountRecorder/Utils/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LineBotAccountRecorder.Dal.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LineBotAccountRecorder.Utils.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddUnitOfWork(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
